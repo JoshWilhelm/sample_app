04bfd6185e6059dcc956ce64a81c166e8b22258f
JoshWilhelm <wilhelm.josh@gmail.com>
2012-09-22 16:34:56 -0400

    5.1 finished

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -20,13 +20,13 @@ describe "Static pages" do
 
     it "should have the h1 'Help'" do
       visit '/static_pages/help'
-      page.should have_selector('h1', :text => 'Help')
+      page.should have_selector('h1', text: 'Help')
     end
 
     it "should have the title 'Help'" do
       visit '/static_pages/help'
       page.should have_selector('title',
-                        :text => "Ruby on Rails Tutorial Sample App | Help")
+                        text: "Ruby on Rails Tutorial Sample App | Help")
     end
   end
 
