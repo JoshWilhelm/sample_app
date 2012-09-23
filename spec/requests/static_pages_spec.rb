b3417d3764769ce802942d0479147627c4d4b45e
JoshWilhelm <wilhelm.josh@gmail.com>
2012-09-23 16:40:46 -0400

    Finished 5.6 exercise 2

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -41,4 +41,19 @@ describe "Static pages" do
 
     it_should_behave_like "all static pages"
   end
+
+	it "should have the right links on the layout" do
+    visit root_path
+    click_link "About"
+    page.should have_selector 'title', text: full_title('About Us')
+    click_link "Help"
+    page.should # fill in
+    click_link "Contact"
+    page.should # fill in
+    click_link "Home"
+    click_link "Sign up now!"
+    page.should # fill in
+    click_link "sample app"
+    page.should # fill in
+  end
 end
