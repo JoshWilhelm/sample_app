0122154b5d978270b7516f315e00aff0702e7a66
JoshWilhelm <wilhelm.josh@gmail.com>
2012-10-31 00:22:48 -0400

    finished 9.2, on to 9.3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,14 +3,13 @@ class SessionsController < ApplicationController
 	def new
   end
 
-  def create
-		user = User.find_by_email(params[:email].downcase)
+	def create
+    user = User.find_by_email(params[:email].downcase)
     if user && user.authenticate(params[:password])
-			session[:user] = user.id
-			sign_in user
-      redirect_to user
+      sign_in user
+      redirect_back_or user
     else
-      flash.now[:error] = 'Invalid email/password combination' 
+      flash.now[:error] = 'Invalid email/password combination'
       render 'new'
     end
   end
