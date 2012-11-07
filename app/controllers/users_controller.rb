617d627077a560d7a7daafb9f856b54d7997e9a4
JoshWilhelm <wilhelm.josh@gmail.com>
2012-11-06 19:14:51 -0500

    Finished chapter 9 exercise 6

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,18 +8,26 @@ class UsersController < ApplicationController
   end	
 
   def new
-		@user = User.new
+		if signed_in?
+			redirect_to(root_path)
+		else
+			@user = User.new
+		end
   end
 
 	def create
-    @user = User.new(params[:user])
-    if @user.save
-			sign_in @user
-			flash[:success] = "Welcome to the Sample App!"
-			redirect_to @user
-    else
-      render 'new'
-    end
+		if signed_in?
+			redirect_to(root_path)
+		else
+    	@user = User.new(params[:user])
+    	if @user.save
+				sign_in @user
+				flash[:success] = "Welcome to the Sample App!"
+				redirect_to @user
+    	else
+      	render 'new'
+    	end
+		end
   end
 	
 	def destroy
